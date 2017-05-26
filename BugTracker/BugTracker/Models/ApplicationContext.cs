bd23cb7288a8f7d49515ceb27078159f0bccd971
kindiro123 <kindiro1234@gmail.com>
2017-05-26 22:43:23 +0300

    add models

--- BugTracker/BugTracker/Models/ApplicationContext.cs
+++ BugTracker/BugTracker/Models/ApplicationContext.cs
@@ -1,24 +1,94 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
-using System;
+﻿using BugTracker.Models.Enums;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace BugTracker.Models
 {
-    public class ApplicationContext : DbContext
+    
+
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
-        public ApplicationContext() : base("bugtrackerDB") { }
- 
-        public static ApplicationContext Create()
+        public ApplicationDbContext()
+            : base("bugtrackerDB", throwIfV1Schema: false)
         {
-            return new ApplicationContext();
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return  new ApplicationDbContext();
         }
 
         public System.Data.Entity.DbSet<BugTracker.Models.Issue> Issues { get; set; }
-
         public System.Data.Entity.DbSet<BugTracker.Models.Project> Projects { get; set; }
-      
+    }
+
+    public class Project
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Customer { get; set; }
+
+        public int? Manager { get; set; }
+
+        public virtual List<Issue> Issues { get; set; }
+
+        public Project()
+        {
+            Issues = new List<Issue>();
+        }
+
+    }
+
+    public class Issue
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Number { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public IssueStatuses Status { get; set; }
+
+        public int? UserId { get; set; }
+
+        public User User { get; set; }
+
+        public int? ProjectId { get; set; }
+
+        public Project Project { get; set; }
+
+    }
+
+    public class User : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync
+                                    (UserManager<User> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this,
+                                    DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+
+        public virtual List<Issue> Issues { get; set; }
+
+        public User()
+        {
+            Issues = new List<Issue>();
+        }
+
     }
 }
---
+++ BugTracker/BugTracker/Models/BugTrackerViewModels.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace BugTracker.Models
+{
+    public class IssueCreateModel
+    {
+
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "User")]
+        public List<string> User { get; set; }
+
+        [Required]
+        [Display(Name = "Price")]
+        public string Price { get; set; }
+
+    }
+
+    public class IssueViewModel
+    {
+
+        public int? Id { get; set; }
+
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+
+        [Display(Name = "Users")]
+        public List<string> Users { get; set; }
+
+        [Display(Name = "Status")]
+        public string Status { get; set; }
+
+
+        [Display(Name = "Price")]
+        public string Price { get; set; }
+    }
+
+    public class ProjectCreateModel
+    {
+
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Customer")]
+        public string Customer { get; set; }
+
+
+        [Required]
+        [Display(Name = "Manager")]
+        public string Manager { get; set; }
+
+    }
+
+    public class ProjectViewModel
+    {
+
+        public int? Id { get; set; }
+
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+
+        [Display(Name = "Customer")]
+        public string Customer { get; set; }
+
+        [Display(Name = "Manager")]
+        public string Manager { get; set; }
+
+        public List<Issue> Issues { get; set; }
+
+
+    }
+}
